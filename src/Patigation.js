f16ebeedd0f94a783fb58a89745221895436d953
Binh <binhbg14@gmail.com>
2020-12-28 04:50:35 +0700

    fix patigation

--- src/Patigation.js
+++ src/Patigation.js
@@ -3,17 +3,27 @@ import './Patigation.css';
 
 function Patigation({setCurrentPage }){
     const [limitPage, setLimitPage] = useState(1);
+    const changeLimitPage = () => {
+        console.log(limitPage);
+        setLimitPage(limitPage+5);
+    }
+    const changeLimitPage2 = () => {
+        console.log(limitPage);
+        setLimitPage(limitPage-5);
+    }
     useEffect(() => {
-        
-    },[])
+        console.log(limitPage);
+    },[limitPage])
     return(
         <div className="patigation">
+            {limitPage > 1 ?
+            <button onClick={changeLimitPage2}>{'<' }</button> :null}
             <button onClick={setCurrentPage(limitPage)} >{limitPage}</button>
             <button onClick={setCurrentPage(limitPage+1)} >{limitPage+1}</button>
             <button onClick={setCurrentPage(limitPage+2)} >{limitPage+2}</button>
             <button onClick={setCurrentPage(limitPage+3)} >{limitPage+3}</button>
             <button onClick={setCurrentPage(limitPage+4)} >{limitPage+4}</button>
-            <button>></button>
+            <button onClick={changeLimitPage}>></button>
         </div>
     )
 }

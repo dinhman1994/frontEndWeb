d202c3d3511296749b56db92bc5d5f764d94d2e4
ntdat041 <ntdat041@gmail.com>
2020-12-27 15:04:14 +0700

    update footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,13 @@
 import React from 'react'
-
+import "./Footer.css";
 function Footer (){
     return(
-        <div>
-            <footer class="tm-footer row tm-mt-small">
+        <div class="footer">
+            <footer class="tm-footer row tm-mt-small">    
                 <div class="col-12 font-weight-light">
-                    <img src="/footerTheme.jpg" alt="OKKKKK"/>
-                    <p class="text-center text-white mb-0 px-4 small">
-                        Design: Group 11
-                    </p>
+                    <p>Made in Hanoi</p>
+                    <p>@ Copyright 2020 Group 11. All Rights Reserved </p>
+                    
                 </div>
             </footer>
         </div>
